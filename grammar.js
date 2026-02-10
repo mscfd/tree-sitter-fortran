eb1ea47e1a09b8b80a004751e92bb80786ef648f
Peter Hill <peter.hill@york.ac.uk>
2026-02-10 13:27:10 +0000

    Fix issue with multiple preprocessor statements before a module
    Fixes #180

--- grammar.js
+++ grammar.js
@@ -177,7 +177,7 @@ module.exports = grammar({
       token.immediate(/\r?\n/),
     ),
 
-    ...preprocIf('', $ => repeat($._top_level_item)),
+    ...preprocIf('', $ => repeat($._top_level_item), 4),
     ...preprocIf('_in_module', $ => seq(
       repeat($._specification_part),
       optional($.internal_procedures)
